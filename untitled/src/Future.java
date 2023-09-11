ac180c795287540ee3c8cadf41610b40ee806c6f
Dani <szabo.f.daniel@gmail.com>
2023-09-11 16:59:18 +0200

    Resolved merge conflict in Future.java

--- untitled/src/Future.java
+++ untitled/src/Future.java
@@ -1,11 +1,5 @@
 public class Future {
-<<<<<<< HEAD
-
-    private String astrology = "Dragon";
-    public void future(String astrology){
-=======
-    public void future(String astrology, String astrology2){
->>>>>>> 250c760 (idk)
+    public void future(String astrology, String astrology2) {
         System.out.println(astrology);
     }
 }
