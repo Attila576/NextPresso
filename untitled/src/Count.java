c08fb59fc2d08124da8a49ab7ac9699e1ab1d770
HanaZah <hana.zahalkova1992@gmail.com>
2023-09-11 16:38:57 +0200

    b5

--- untitled/src/Count.java
+++ untitled/src/Count.java
@@ -1,5 +1,5 @@
 public class Count {
-    public void count(int age) {
-        System.out.println("8");
+    public void count(int age, int month) {
+        System.out.println("age: " + age + ", birth month: " + month);
     }
 }
